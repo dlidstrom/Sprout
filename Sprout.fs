6d9198f38d6c402742cb4707708d09b100de6544
Daniel Lidstrom <dlidstrom@gmail.com>
2025-06-20 08:28:09 +0200

    refactor: reorganize test reporter structure and improve logging functionality

--- Sprout.fs
+++ Sprout.fs
@@ -1,5 +1,4 @@
 module Sprout
-open System.Diagnostics
 
 let mutable info: string -> unit = ignore
 let mutable debug: string -> unit = ignore
@@ -35,12 +34,6 @@ type ItBuilder(name: string) =
 
 let it name = ItBuilder name
 let pending name = It.Pending name
-
-module AnsiColours =
-  let green = "\u001b[32m"
-  let red = "\u001b[31m"
-  let grey = "\u001b[90m"
-  let reset = "\u001b[0m"
 
 type Describe = {
   Name: string
@@ -97,66 +90,73 @@ type TestResult =
   | Pending of Path * string
 
 type ITestReporter =
-  abstract BeginSuite : name:string * level:int -> unit
-  abstract ReportResult : result:TestResult * level:int -> unit
-  abstract EndSuite : name:string * level:int -> unit
-  abstract Info : message:string * indent:string * indentCount:int -> unit
-  abstract Debug : message:string * indent:string * indentCount:int -> unit
+  abstract BeginSuite : name:string * path:Path -> unit
+  abstract ReportResult : result:TestResult * path:Path -> unit
+  abstract EndSuite : name:string * path:Path -> unit
+  abstract Info : message:string * path:Path -> unit
+  abstract Debug : message:string * path:Path -> unit
   abstract End : TestResult list -> unit
 
-type ConsoleReporter() =
-  let sw = Stopwatch.StartNew()
-  interface ITestReporter with
-    member _.BeginSuite(name, level) =
-      let indent = String.replicate (level * 2) " "
-      printfn $"{indent}{AnsiColours.green}{name}{AnsiColours.reset}"
+module Reporters =
+  open System.Diagnostics
 
-    member _.ReportResult(result, level) =
-      let indent = String.replicate (level * 2) " "
-      match result with
-      | Passed (_, name) ->
-          printfn $"%s{indent}%s{AnsiColours.green}  ✅ passed: %s{name}%s{AnsiColours.reset}"
-      | Failed (_, name, ex) ->
-          printfn $"%s{indent}%s{AnsiColours.red}  ❌ failed: %s{name} - %s{ex.Message}%s{AnsiColours.reset}"
-      | Pending (_, name) ->
-          printfn $"%s{indent}%s{AnsiColours.grey}  ❔ pending: %s{name}%s{AnsiColours.reset}"
+  module AnsiColours =
+    let green = "\u001b[32m"
+    let red = "\u001b[31m"
+    let grey = "\u001b[90m"
+    let white = "\u001b[37m"
+    let reset = "\u001b[0m"
 
-    member _.EndSuite(_, _) = ()
-    member _.Debug(message: string, indent: string, indentCount: int): unit =
-      let indent = String.replicate (indentCount * 2) indent
-      printfn $"%s{indent}%s{AnsiColours.grey}%s{message}%s{AnsiColours.reset}"
-    member _.Info(message: string, indent: string, indentCount: int): unit =
-      let indent = String.replicate (indentCount * 2) indent
-      printfn $"%s{indent}%s{AnsiColours.grey}%s{message}%s{AnsiColours.reset}"
-    member _.End(testResults: TestResult list): unit =
-      printfn $"got %d{List.length testResults} test results"
-      testResults |> List.iter (function
-        // | Passed (path, name) ->
-        //   let pathString = String.concat " / " path.Value
-        //   context.Log $"Test passed: %s{AnsiColours.green}%s{pathString} - %s{name}%s{AnsiColours.reset}"
-        | Failed (path, name, ex) ->
-          let pathString = String.concat " / " path.Value
-          printfn $"Test failed: %s{AnsiColours.red}%s{pathString} / %s{name} - %s{ex.Message}%s{AnsiColours.reset}"
-        // | Pending (path, name) ->
-        //   let pathString = String.concat " / " path.Value
-        //   context.Log $"Test pending: %s{AnsiColours.grey}%s{pathString} / %s{name}%s{AnsiColours.reset}")
-        | _ -> ())
+  type ConsoleReporter() =
+    let sw = Stopwatch.StartNew()
+    let indent (path: Path) = String.replicate ((path.Length - 1) * 2) " "
+    interface ITestReporter with
+      member _.BeginSuite(name, path) =
+        let indent = indent path
+        printfn $"%s{indent}{AnsiColours.green}{name}{AnsiColours.reset}"
 
-      // Count results
-      let passedCount = testResults |> List.filter (function Passed _ -> true | _ -> false) |> List.length
-      let failedCount = testResults |> List.filter (function Failed _ -> true | _ -> false) |> List.length
-      let pendingCount = testResults |> List.filter (function Pending _ -> true | _ -> false) |> List.length
+      member _.ReportResult(result, path) =
+        let indent = indent path
+        match result with
+        | Passed (_, name) ->
+            printfn $"%s{indent}%s{AnsiColours.green}  ✅ passed: %s{name}%s{AnsiColours.reset}"
+        | Failed (_, name, ex) ->
+            printfn $"%s{indent}%s{AnsiColours.red}  ❌ failed: %s{name} - %s{ex.Message}%s{AnsiColours.reset}"
+        | Pending (_, name) ->
+            printfn $"%s{indent}%s{AnsiColours.grey}  ❔ pending: %s{name}%s{AnsiColours.reset}"
 
-      printfn $"Summary: {passedCount} passed, {failedCount} failed, {pendingCount} pending"
-      printfn $"Total time: %s{sw.Elapsed.ToString()}"
+      member _.EndSuite(_, _) = ()
+      member _.Debug(message: string, path: Path): unit =
+        let indent = indent path
+        printfn $"%s{indent}%s{AnsiColours.grey}%s{message}%s{AnsiColours.reset}"
+      member _.Info(message: string, path: Path): unit =
+        let indent = indent path
+        printfn $"%s{indent}%s{AnsiColours.white}%s{message}%s{AnsiColours.reset}"
+      member _.End(testResults: TestResult list): unit =
+        let testFailures = testResults |> List.filter (function Failed _ -> true | _ -> false)
+        if not (List.isEmpty testFailures) then
+          printfn $"There were %d{List.length testFailures} test failures:"
+        else
+          printfn $"All tests passed!"
+        testResults |> List.iter (function
+          | Failed (path, name, ex) ->
+            let pathString = String.concat " / " path.Value
+            printfn $"- %s{AnsiColours.red}%s{pathString} / %s{name} - %s{ex.Message}%s{AnsiColours.reset}"
+          | _ -> ())
+
+        // Count results
+        let passedCount = testResults |> List.filter (function Passed _ -> true | _ -> false) |> List.length
+        let failedCount = testResults |> List.filter (function Failed _ -> true | _ -> false) |> List.length
+        let pendingCount = testResults |> List.filter (function Pending _ -> true | _ -> false) |> List.length
+
+        printfn $"Summary: {passedCount} passed, {failedCount} failed, {pendingCount} pending"
+        printfn $"Total time: %s{sw.Elapsed.ToString()}"
 
 type TestContext = {
   Path: Path
   ParentBeforeHooks: HookFunction list
   ParentAfterHooks: HookFunction list
   Reporter: ITestReporter
-  Indent: string
-  IndentCount: int
   Log: string -> unit
 }
 with
@@ -164,34 +164,37 @@ with
     Path = Path []
     ParentBeforeHooks = []
     ParentAfterHooks = []
-    Indent = " "
-    IndentCount = 2
-    Reporter = ConsoleReporter() :> ITestReporter
+    Reporter = Reporters.ConsoleReporter() :> ITestReporter
     Log = printfn "%s"
   }
 
 type TestSuiteRunner = Describe -> TestContext -> unit
+type private LogLevel = Debug of string | Info of string
 
-let runTestCase path (testCase: It): TestResult =
-  match testCase.Body with
-  | Some body ->
-    try
-      body()
-      Passed (path, testCase.Name)
-    with ex ->
-      Failed (path, testCase.Name, ex)
-  | None ->
-    Pending (path, testCase.Name)
-
-let rec doRunTestSuite (suite: Describe) (context: TestContext) =
+let private runTestCase path (testCase: It) beforeHooks afterHooks =
   // setup logging functions
   let info', debug' = info, debug
   use _ = { new System.IDisposable with
     member _.Dispose() = info <- info'; debug <- debug' }
-  info <- fun s -> context.Reporter.Info(s, context.Indent, context.IndentCount)
-  debug <- fun s -> context.Reporter.Debug(s, context.Indent, context.IndentCount)
+  let logs = ResizeArray<LogLevel>()
+  info <- fun s -> logs.Add (Info s)
+  debug <- fun s -> logs.Add (Debug s)
+  beforeHooks |> Seq.iter (fun hookFunction -> hookFunction())
+  let result =
+    match testCase.Body with
+    | Some body ->
+      try
+        body()
+        Passed (path, testCase.Name)
+      with ex ->
+        Failed (path, testCase.Name, ex)
+    | None ->
+      Pending (path, testCase.Name)
+  afterHooks |> Seq.iter (fun hookFunction -> hookFunction())
+  result, logs
 
-  context.Reporter.BeginSuite(suite.Name, context.IndentCount)
+let rec private doRunTestSuite (suite: Describe) (context: TestContext): TestResult list =
+  context.Reporter.BeginSuite(suite.Name, context.Path)
 
   let beforeHooks, afterHooks =
     suite.Each
@@ -205,13 +208,15 @@ let rec doRunTestSuite (suite: Describe) (context: TestContext) =
 
   let testResults = [
     for testCase in suite.TestCases do
-      beforeHooks |> Seq.iter (fun hookFunction -> hookFunction())
-      runTestCase context.Path testCase
-      afterHooks |> Seq.iter (fun hookFunction -> hookFunction())
+      runTestCase context.Path testCase beforeHooks afterHooks
   ]
 
-  for result in testResults do
-    context.Reporter.ReportResult(result, context.Path.Length)
+  for result, logs in testResults do
+    for log in logs do
+      match log with
+      | Info message -> context.Reporter.Info(message, context.Path)
+      | Debug message -> context.Reporter.Debug(message, context.Path)
+    context.Reporter.ReportResult(result, context.Path)
 
   let childrenResults =
     suite.Children
@@ -224,23 +229,22 @@ let rec doRunTestSuite (suite: Describe) (context: TestContext) =
       doRunTestSuite
         child
         childContext)
-  testResults @ List.concat childrenResults
+  let allResults = (testResults |> List.map fst) @ List.concat childrenResults
+  allResults
 
-let runTestSuite (sb: Describe) (context: TestContext) =
+let runTestSuiteWithContext (sb: Describe) (context: TestContext) =
   let testResults = doRunTestSuite sb { context with Path = Path (context.Path.Value @ [sb.Name]) }
-  context.Reporter.EndSuite(sb.Name, context.Path.Length)
+  context.Reporter.EndSuite(sb.Name, context.Path)
   context.Reporter.End testResults
-  ()
+
+let runTestSuite (sb: Describe) = runTestSuiteWithContext sb TestContext.Empty
 
 [<AutoOpen>]
 module Constraints =
   let shouldEqual expected actual =
     if expected <> actual then
       failwithf "Expected %A but got %A" expected actual
-    else
-      info $"Expected %A{expected} and got %A{actual} - test passed"
+
   let shouldNotEqual unexpected actual =
     if unexpected = actual then
       failwithf "Expected not to be %A but got %A" unexpected actual
-    else
-      info $"Expected not to be %A{unexpected} and got %A{actual} - test passed"
